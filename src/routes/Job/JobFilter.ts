0fe6f052be70b27aece2be8a475d8bcfa9784afe
gellert5225 <gellert.li1995@gmail.com>
2023-09-28 21:33:39 +0000

    job filters

---
+++ src/routes/Job/JobFilter.ts
@@ -0,0 +1,83 @@
+export const JobTypes = [
+	{
+		id: "projct-checkbox",
+		name: "Project"
+	},
+	{
+		id: "mentor-checkbox",
+		name: "Mentor/Tutor"
+	},
+	{
+		id: "research-checkbox",
+		name: "Research"
+	},
+	{
+		id: "grader-checkbox",
+		name: "Grader"
+	},
+	{
+		id: "teaching-checkbox",
+		name: "Teaching"
+	},
+	{
+		id: "other-checkbox",
+		name: "Other"
+	}
+];
+
+export const ExperienceLevel = [
+	{
+		id: "entry-checkbox",
+		name: "Entry"
+	},
+	{
+		id: "senior-checkbox",
+		name: "Senior"
+	},
+	{
+		id: "junior-checkbox",
+		name: "Junior"
+	},
+	{
+		id: "Graduate-checkbox",
+		name: "Graduate"
+	}
+];
+
+export const DatePosted = [
+	{
+		id: "anydate-radio",
+		name: "Any Time"
+	},
+	{
+		id: "week-radio",
+		name: "Past Week"
+	},
+	{
+		id: "day-radio",
+		name: "Past Day"
+	},
+	{
+		id: "Month-radio",
+		name: "Past Month"
+	}
+];
+
+export const PayRate = [
+	{
+		id: "anyrate-radio",
+		name: "Any"
+	},
+	{
+		id: "20-radio",
+		name: "$20+"
+	},
+	{
+		id: "10-radio",
+		name: "$10+"
+	},
+	{
+		id: "1030-radio",
+		name: "$30+"
+	}
+];
--- src/routes/Job/JobPage.tsx
+++ src/routes/Job/JobPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from "../../context/AuthContext";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { JobTypes, ExperienceLevel, DatePosted, PayRate } from './JobFilter';
 
 const JobPage = () => {
 	const { removeItem } = useLocalStorage();
@@ -27,12 +28,62 @@ const JobPage = () => {
 					<hr className="w-full h-[1px] my-2 bg-gray-100 border-0 rounded md:my-2 dark:bg-gray-700"></hr>
 					<div>
 						<h1 className='text-md text-white font-semibold'>Job Type</h1>
-						<div className='flex flex-wrap'>
-							<div className="flex items-center">
-									<input id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-0 dark:bg-gray-700 dark:border-gray-600" />
-									<label htmlFor="checked-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
-							</div>
+						<div className='flex flex-wrap mt-1'>
+							{JobTypes.map((v, i) => (
+								<div key={i} className="flex items-center w-1/2 p-1">
+									<input id={v.id} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-transparent border-lightGray rounded-sm outline-none focus:ring-offset-0 focus:outline-none focus:ring-0 dark:bg-transparent dark:border-lightGray" />
+									<label htmlFor={v.id} className="ml-2 text-md font-medium text-lightGray dark:text-lightGray">{v.name}</label>
+								</div>
+							))}
 						</div>
+					</div>
+					<hr className="w-full h-[1px] my-2 bg-gray-100 border-0 rounded md:my-2 dark:bg-gray-700"></hr>
+					<div>
+						<h1 className='text-md text-white font-semibold'>Experience Level</h1>
+						<div className='flex flex-wrap mt-1'>
+							{ExperienceLevel.map((v, i) => (
+								<div key={i} className="flex items-center w-1/2 p-1">
+									<input id={v.id} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-transparent border-lightGray rounded-sm outline-none focus:ring-offset-0 focus:outline-none focus:ring-0 dark:bg-transparent dark:border-lightGray" />
+									<label htmlFor={v.id} className="ml-2 text-md font-medium text-lightGray dark:text-lightGray">{v.name}</label>
+								</div>
+							))}
+						</div>
+					</div>
+					<hr className="w-full h-[1px] my-2 bg-gray-100 border-0 rounded md:my-2 dark:bg-gray-700"></hr>
+					<div>
+						<h1 className='text-md text-white font-semibold'>Date Posted</h1>
+						<div className='flex flex-wrap mt-1'>
+							{DatePosted.map((v, i) => (
+								<div key={i} className="flex items-center w-1/2 p-1">
+									<input id={v.id} type="radio" value="" name="radio-date" className="w-4 h-4 text-blue-600 bg-transparent border-lightGray outline-none focus:ring-offset-0 focus:outline-none focus:ring-0 dark:bg-transparent dark:border-lightGray" />
+									<label htmlFor={v.id} className="ml-2 text-md font-medium text-lightGray dark:text-lightGray">{v.name}</label>
+								</div>
+							))}
+						</div>
+					</div>
+					<hr className="w-full h-[1px] my-2 bg-gray-100 border-0 rounded md:my-2 dark:bg-gray-700"></hr>
+					<div>
+						<h1 className='text-md text-white font-semibold'>Pay Rate / hr</h1>
+						<div className='flex flex-wrap mt-1'>
+							{PayRate.map((v, i) => (
+								<div key={i} className="flex items-center w-1/2 p-1">
+									<input id={v.id} type="radio" value="" name="radio-salary" className="w-4 h-4 text-blue-600 bg-transparent border-lightGray outline-none focus:ring-offset-0 focus:outline-none focus:ring-0 dark:bg-transparent dark:border-lightGray" />
+									<label htmlFor={v.id} className="ml-2 text-md font-medium text-lightGray dark:text-lightGray">{v.name}</label>
+								</div>
+							))}
+						</div>
+					</div>
+					<hr className="w-full h-[1px] my-2 bg-gray-100 border-0 rounded md:my-2 dark:bg-gray-700"></hr>
+					<div className='flex justify-between'>
+						<h1 className='text-md text-white font-semibold'>Remote</h1>
+						<label className="relative inline-flex items-center cursor-pointer">
+							<input type="checkbox" value="" className="sr-only peer" />
+							<div className="w-11 h-6 bg-lightGray peer-focus:ring-offset-0 rounded-full peer dark:bg-lightGray peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+						</label>
+					</div>
+					<div className='flex justify-around mt-4'>
+						<button type="button" className="text-white bg-blue-700 hover:bg-blue-800 -focus:ring-offset-0 font-medium rounded-md text-md px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700">Apply Filters</button>
+						<button type="button" className="text-md font-bold text-center text-lightGray bg-transparent hover:bg-transparent focus:ring-0 focus:outline-none">Cancel</button>
 					</div>
 				</div>
 				<div className='grow px-1 sm:px-8'>
